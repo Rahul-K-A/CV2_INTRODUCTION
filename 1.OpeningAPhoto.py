1b4734dfe351a9b111c04f50eb1c492f271774d5
Rahul K.A <karahul209@gmail.com>
2021-05-31 10:08:24 +0530

    Clarified comments

--- 1.OpeningAPhoto.py
+++ 1.OpeningAPhoto.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy
 #reads image
+#cv2.IMREAD_UNCHANGED tells system to read transparent pixels as they are
+#this prevents artefacts from occuring in the output
 img=cv2.imread('Resources\Lena.jpg',cv2.IMREAD_UNCHANGED)
 #shows image in new window (window name,image)
 cv2.imshow("TEST IMAGE",img)
